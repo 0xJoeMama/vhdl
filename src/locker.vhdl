ed73f7155e51f0def6269b51fd5c5ee686d0af23
0xJoeMama <0xjoemama@gmail.com>
2024-10-22 18:38:28 +0300

    feat: initial implementation

---
+++ src/locker.vhdl
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity locker is
+  port(
+    digit0, digit1, digit2, digit3: in STD_LOGIC; -- 0 = LSB
+    lock_out: out STD_LOGIC
+  );
+end entity;
+
+-- sdi2400168 therefore, we need 8 which in binary is 1000
+-- to match 1000 with the 4 signals we must do the following:
+-- - digit3 * !digit2 * !digit1 * !digit0
+architecture Dataflow of locker is
+begin
+  lock_out <= digit3 and (not digit2) and (not digit1) and (not digit0);
+end architecture;
---
+++ src/locker_tb.vhdl
@@ -0,0 +1,83 @@
+LIBRARY ieee; USE ieee.std_logic_1164.ALL;
+use std.textio.all;
+ 
+entity locker_tb IS
+end locker_tb;
+ 
+architecture behavior OF locker_tb IS 
+    -- Component Declaration for the Unit Under Test (UUT)
+  component locker
+    port(
+      digit3, digit2, digit1, digit0   : in std_logic;
+      lock_out : out std_logic
+    );
+  end component;
+
+  --Inputs
+  signal sw3, sw2, sw1, sw0 : std_logic;
+  --Outputs
+  signal led0 : std_logic;
+begin
+  uut: locker port map (
+    digit0 => sw0,
+    digit1 => sw1,
+    digit2 => sw2,
+    digit3 => sw3,
+    lock_out => led0
+  );
+
+  -- Test process
+  test_proc: process
+  begin		
+    sw3<='0';sw2<='0';sw1<='0';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='0';sw1<='0';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='0';sw1<='1';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='0';sw1<='1';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='1';sw1<='0';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='1';sw1<='0';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='1';sw1<='1';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='0';sw2<='1';sw1<='1';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='0';sw1<='0';sw0<='0';wait for 20 ns;
+    assert led0 = '1'
+    report "bad result" severity error;
+    sw3<='1';sw2<='0';sw1<='0';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='0';sw1<='1';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='0';sw1<='1';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='1';sw1<='0';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='1';sw1<='0';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='1';sw1<='1';sw0<='0';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+    sw3<='1';sw2<='1';sw1<='1';sw0<='1';wait for 20 ns;
+    assert led0 = '0'
+    report "bad result" severity error;
+
+    wait;
+  end process;
+end behavior;
